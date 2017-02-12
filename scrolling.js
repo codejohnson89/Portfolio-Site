a81b9a3bdba0804c12e9c5231086af6667f0e479
Micah Johnson <micahjj1989@gmail.com>
2017-02-11 20:44:38 -0600

    Update to the scrolling.js
    moved the scrollspy for bootstrap

--- scrolling.js
+++ scrolling.js
@@ -2,4 +2,18 @@
     $(window).on("load resize", function () {
         $(".fill-screen").css("height", window.innerHeight);
     });
+
+    // scrollspy for bootstrap
+    $('body').scrollspy({
+        target: '.navbar'
+    });
+
+    // smooth scrolling
+    $('nav a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
 });
