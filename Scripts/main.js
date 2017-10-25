bd6b5464b83493a9038d8d0beed0bd9b7ae077ce
Micah Johnson <info@micahjjohnson.com>
2017-10-25 14:07:24 -0500

    Changes to the navbar and skills section
    Changes to the navbar and skills section

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,50 +1,66 @@
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 100) {
-        $("#logo").attr("src", "img/logo-white.png");
-        $(".navbar").addClass("show");
-        $("#navlist").addClass("slideInRight");
-        $("#navlist").removeClass("pull-right");
-    } else {
-        $("#logo").attr("src", "img/logo.png");
-        $(".navbar").removeClass("show");
-        $("#navlist").addClass("pull-right");
-        $("#navlist").removeClass("slideInRight");
-
-    }
+(function() {
 
     $(window).scroll(function() {
-        if ($(window).scrollTop() > 1700) {
-            $("#skills").addClass("lightSpeedIn");
-            $("#skills").removeClass("hidden");
-        }
-    });
-
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 800) {
-            $("#okc .home-city").addClass("fadeInUpBig");
-            $("#okc .home-city").removeClass("hidden");
-        }
-    });
-
-
-});
-
-function mobileResize() {
-    if (0 == 0) {
-        if ($(window).width() < 415) {
-            $("#mainImg").removeClass("bg-header");
-            console.log("1");
+        if ($(window).scrollTop() > 100) {
+            $("#logo").attr("src", "img/logo-white.png");
+            $(".navbar").addClass("show");
+            $("#navlist").addClass("slideInRight");
+            $("#navlist").removeClass("pull-right");
         } else {
-            $("#mainImg").addClass("bg-header");
-            console.log("2");
-        }
-    }
-}
-$(".social .fa").hover(function() {
-        $(this).addClass("bounce");
-    },
-    function() {
-        $(this).removeClass("bounce");
-    });
+            $("#logo").attr("src", "img/logo.png");
+            $(".navbar").removeClass("show");
+            $("#navlist").addClass("pull-right");
+            $("#navlist").removeClass("slideInRight");
 
-mobileResize();
+        }
+
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 1700) {
+                $("#skills").addClass("lightSpeedIn");
+                $("#skills").removeClass("hidden");
+            }
+        });
+
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 800) {
+                $("#okc .home-city").addClass("fadeInUpBig");
+                $("#okc .home-city").removeClass("hidden");
+            }
+        });
+
+
+    });
+    $(".social .fa").hover(function() {
+            $(this).addClass("bounce");
+        },
+        function() {
+            $(this).removeClass("bounce");
+        });
+
+    function load() {
+        $(window).ready(function() {
+            if ($(window).width() < 992) {
+                $(".right").removeClass("text-right");
+                $(".right").addClass("text-center");
+            } else {
+                $(".right").removeClass("text-center");
+                $(".right").addClass("text-right");
+            }
+        });
+
+
+        $(window).ready(function() {
+            if ($(window).width() < 992) {
+                $(".left").removeClass("text-left");
+                $(".left").addClass("text-center");
+            } else {
+                $(".left").removeClass("text-center");
+                $(".left").addClass("text-left");
+            }
+        });
+    }
+
+    console.log("working");
+
+    load();
+})();
